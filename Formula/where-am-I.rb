b270b2871e2bd915b3f05028444657decd05e625
tatoalo <apogliaghi@gmail.com>
2021-04-20 09:10:57 +0200

    Release 1.0.1

--- Formula/where-am-I.rb
+++ Formula/where-am-I.rb
@@ -1,11 +1,11 @@
 class WhereAmI < Formula
     desc "Component Frameworks Demo"
     homepage "https://github.com/tatoalo/where-am-I"
-    url "https://github.com/tatoalo/where-am-I/releases/latest/download/run-me-mac.tar.gz"
-    sha256 "5b308f474e8d94b37b26e7f251f170cd2d25f96cf511aebeae699997b303eaaf"
-    version "1.0.0"
+    url "https://github.com/tatoalo/where-am-I/releases/latest/download/where-am-I.tar.gz"
+    sha256 "9a460b49d5d63bbe38607e2222f7e0f259e86dc46a03d64fc49fb0addffa9a7a"
+    version "1.0.1"
   
     def install
-      bin.install "run_me"
+      bin.install "where_am_I"
     end
   end
