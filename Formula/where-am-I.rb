4483ecb560a2ddc9248c69677c59149139e93047
tatoalo <apogliaghi@gmail.com>
2022-02-08 23:04:28 +0100

    Fixed build number

--- Formula/where-am-I.rb
+++ Formula/where-am-I.rb
@@ -3,7 +3,7 @@ class WhereAmI < Formula
     homepage "https://github.com/tatoalo/where-am-I"
     url "https://github.com/tatoalo/where-am-I/releases/latest/download/where-am-I.tar.gz"
     sha256 "c25eca83eaa008ada9e3a0bc0198bddd6fc61f985f4039b58d7acdce8559443d""
-    version "1.1"
+    version "1.1.0"
   
     def install
       bin.install "where_am_I"
