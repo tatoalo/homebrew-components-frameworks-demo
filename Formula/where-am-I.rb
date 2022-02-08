bad10b6c164490c47fa504e08eac60fafd8ec7b4
tatoalo <apogliaghi@gmail.com>
2022-02-08 21:09:43 +0100

    Updated formula with v1.1

--- Formula/where-am-I.rb
+++ Formula/where-am-I.rb
@@ -2,8 +2,8 @@ class WhereAmI < Formula
     desc "Component Frameworks Demo"
     homepage "https://github.com/tatoalo/where-am-I"
     url "https://github.com/tatoalo/where-am-I/releases/latest/download/where-am-I.tar.gz"
-    sha256 "9a460b49d5d63bbe38607e2222f7e0f259e86dc46a03d64fc49fb0addffa9a7a"
-    version "1.0.1"
+    sha256 "b251075ae9c69741e2b8b0a35306e49c2375d0f65ced3b881b8c07c766095d66"
+    version "1.1"
   
     def install
       bin.install "where_am_I"
